2277cace338f7ab55025b270f7a83a7bcc1dbbe5
samjw <wagnerjsamuel@gmail.com>
2019-01-14 18:43:23 -0500

    functional, needs tweaking but otherwise good to go.

--- liri.js
+++ liri.js
@@ -1,154 +1,163 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var axios = require("axios");
-var moment = require("moment");
-var Spotify = require("node-spotify-api");
-var fs = require("fs");
 
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-var action = process.argv[2];
+var moment = require('moment');
+moment().format();
 
-switch(action) {
+var axios = require("axios");
+var fs = require("fs");
+
+var command = process.argv[2];
+var value = process.argv[3];
+
+switch (command) {
     case "concert-this":
-    concert();
+    concertThis(value);
     break;
 
     case "spotify-this-song":
-    songify();
+    spotifySong(value);
     break;
 
     case "movie-this":
-    movie();
+    movieThis(value);
     break;
 
     case "do-what-it-says":
-    simon();
+    simon(value);
     break;
-}
 
-function concert() {
-    var nodeArgs = process.argv;
-    var artistName = "";
+    default:
+    console.log("Please choose a valid command.");
+    return;
+};
 
-    for (var i = 3; i < nodeArgs.length; i++) { 
-        if (i > 3 && i < nodeArgs.length) {
-            artistName = artistName + "+" + nodeArgs[i];
-        }
-        else {
-            artistName += nodeArgs[i];
-        }
-    }
-    
-    axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp").
-    then(function(response){
-        for (i=0; i < response.length; i++) {
-            console.log("Venue: " + response.data[i].venue.name);
-            if (response.data[i].venue.region === "") {
-                console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
-            } else {
-                console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.region);
-            }
-            console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY") + "\n");
+function concertThis(value){
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
+    .then(function(response) {
+        for (var i = 0; i < response.value.length; i++) {
+            var datetime = response.value[i].datetime;
+            var dateArr = datetime.split('T');
+
+            console.log( "-------------------------" +
+            "\nVenue Name: " + response.value[i].venue.name +
+            "\nVenue Location: " + response.value[i].venue.city +
+            "\nEvent Date: " + moment(response.value[i].datetime).format("MM/DD/YYYY") + "\n");
+            contentAdded();
         }
     });
 }
 
-function songify() {
+function spotifySong() {
     var nodeArgs = process.argv;
     var songName = "";
-
-    if (nodeArgs.length > 4) {
-        songName = "ace base the sign"
-    }
-    else {
-        for (var i = 3; i < nodeArgs.length; i++) {
-            if (i > 3 && i < nodeArgs.length) {
-                songName = songName + "+" + nodeArgs[i];
-            }
-            else { 
-                songName += nodeArgs[i];
-            }
-        }
-    }
-
-    spotify.search({type: "track", query: songName}, function(err,data) {
-        if (err) {
-            return console.log("error occurred: " + err);
-        }
-
-        var artistName = data.tracks.items[0].artists[0].name;
-
-        for (var i = 1; i < data.tracks.items[0].artists[0].name; i++) {
-            if (data.tracks.items[0].artists.length > 1) {
-                artistName = artistName + ", " + data.tracks.items[0].arists[i].name;
-            }
-        }
-        console.log("Artist(s): " + artistName);
-        console.log("Song Name: " + data.tracks.items[0].name);
-        console.log("Preview: " + data.tracks.items[0].preview_url);
-        console.log("Album: " + data.tracks.items[0].album.name);
-    });
-}
-
-function movie() {
-    var nodeArgs = process.argv;
-    var movieName = "";
-
+  
     if (nodeArgs.length < 4) {
-        movieName = "mr nobody";
+      songName = "ace base the sign"
     }
-
     else {
-        for (var i = 3; i < nodeArgs.length; i++) {
-            if (i > 3 && i < nodeArgs.length) {
-                movieName = movieName + "+" + nodeArgs[i];
-            }
-            else {
-                movieName += nodeArgs[i];
-            }
+      for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+          songName = songName + "+" + nodeArgs[i];
         }
+        else {
+          songName += nodeArgs[i];
+  
+        }
+      }
+    }
+  
+    spotify.search({ type: 'track', query: songName }, function(err, value) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+
+      var artistName = value.tracks.items[0].artists[0].name;
+  
+      // if there are multiple artists on song
+      for (i=1; i < value.tracks.items[0].artists.length; i++) {
+        if (value.tracks.items[0].artists.length > 1) {
+          artistName = artistName + ", " + value.tracks.items[0].artists[i].name;
+        }
+      }
+      console.log("Artist(s): " + artistName + 
+      "\nSong Name: " + value.tracks.items[0].name +
+      "\nPreview: " + value.tracks.items[0].preview_url +
+      "\nAlbum: " + value.tracks.items[0].album.name)
+      contentAdded();
+      });
+  }
+
+
+function movieThis(value){
+    if (!value) {
+        value = "mr nobody";
     }
 
-    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy&tomatoes=true").
-    then(function(response) {
-        console.log("Title: " + response.data.Title);
-        console.log("Release Year: " + response.data.Year);
-        console.log("IMDB Rating: " + response.data.imdbRating);
-        console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
-        console.log("Country: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Cast: " + response.data.Actors);
+    axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
+    .then(function(response){
+        console.log("------------------------" + 
+        "\nMovie Title: " + response.value.Title +
+        "\nYear of Release: " + response.value.Year +
+        "\nIMDB Rating: " + response.value.imdbRating +
+        "\nRotten Tomatoes Rating: " + response.value.Ratings[1].Value +
+        "\nCountry Produced: " + response.value.Country +
+        "\nLanguage: " + response.value.Language +
+        "\nPlot: " + response.value.Plot +
+        "\nCast: " + response.value.Actors);
+        contentAdded();
     });
+
 }
 
-function simon() {
-    fs.readFile("random.txt", "utf8", function(err,data){
+function simon(value){
+    fs.readFile("random.txt", "utf8", function(err, value) {
         if (err) {
-            return console.log(err);
+          return console.log(err);
         }
-
-        data = data.split(",");
-
-
-        action = data[0];
-        process.argv[3] = data[1];
+    
+        // console.log(value);
+        value = value.split(",");
+        // console.log(value);
+        // console.log(value[0]);
+        // console.log(value[1]);
+        
+        action = value[0];
+        process.argv[3] = value[1];
         switch (action) {
-            case "concert-this": 
-            process.argv[3] = data[1].replace(/['"]+/g, '');
-            concert();
+          case "concert-this":
+            // method below removes quotation marks from artist name string
+            process.argv[3] = value[1].replace(/['"]+/g, '')
+            concertThis();
             break;
-
-            case "spotify-this-song":
-            songify();
-
-            case "movie-this":
-            movie();
+        
+          case "spotify-this-song":
+            spotifySong();
+            break;
+        
+          case "movie-this":
+            movieThis();
             break;
         }
-    })
-}
+      });
+    }
 
-
+function contentAdded() {
+    console.log("");
+    console.log("Content Added!");
+    console.log("-----------------------------------\n");
+    appendFile("-----------------------------------\n");
+  }
+  //appendFile function
+  var logvalue = process.argv[2] + ": " + process.argv[3] + ", ";
+  function appendFile() {
+    fs.appendFile("log.txt", logvalue, function(err) {
+      if (err) {
+        console.log(err);
+      } else {}
+    });
+  }
